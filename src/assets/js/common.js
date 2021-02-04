7df467c6f648ff5ac334e48af0cc5c4bfa2919b5
muralidhardubey25@gmail.com <vikas.yadav@sensationsolutions.com>
2021-02-04 13:07:37 +0530

    initial

---
+++ src/assets/js/common.js
@@ -0,0 +1,25 @@
+$(function () {
+    $('.navbar-toggler').click(function () {
+      $('body').toggleClass('noscroll');
+    })
+  });
+  window.onscroll = function () {
+    scrollFunction()
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("movetop").style.display = "block";
+    } else {
+        document.getElementById("movetop").style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
+
+    // When the user scrolls down 20px from the top of the document, show the button
+ 
